15abbdba83f786b6dfd67f3046c0025ccb3c9844
ntjohns1 <nelsontjohns@gmail.com>
2021-10-29 04:08:24 -0400

    isPrime

---
+++ perfectNumbers/perfectNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, sum=0;
+    cout<<"enter n";
+    cin>>n;
+    for (i = 1; i <=n; i++)
+    {
+        if (n%i==0)
+        {
+            sum=sum+i;
+        }   
+        // if the sum of the factors of n is 2*n it is a perfext number
+        if (2*n==sum)
+        {
+            cou<<"Perfect Number";
+        }
+        
+        
+    }
+    return 0;
+}
